6f3019ca2c873e8e80a68c8cf30ef27391170693
Reed Li <shixiliufanglzh@gmail.com>
2020-05-11 22:21:24 +0800

    feat: add component Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Button, { ButtonType, ButtonSize } from './components/Button/button';
 
 interface IShowResult {
   message: string;
@@ -13,7 +14,50 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <code>const A = '1234'</code>
-        <button>hello</button>
+        <Button
+          autoFocus
+          btnType={ButtonType.Primary}
+          size={ButtonSize.Large}
+          disabled={false}
+        >
+          Large Primary
+        </Button>
+        <Button
+          btnType={ButtonType.Danger}
+          size={ButtonSize.Small}
+          disabled={false}
+        >
+          Small Danger
+        </Button>
+        <Button
+          btnType={ButtonType.Default}
+          disabled={false}
+        >
+          Default
+        </Button>
+        <Button
+          btnType={ButtonType.Primary}
+          disabled={true}
+        >
+          disabled Primary
+        </Button>
+        <Button
+          btnType={ButtonType.Link}
+          size={ButtonSize.Large}
+          disabled={true}
+          href="http://www.zhiheng.space"
+        >
+          Large disabled Link
+        </Button>
+        <Button
+          btnType={ButtonType.Link}
+          size={ButtonSize.Small}
+          disabled={false}
+          target="_blank"
+          href="http://www.zhiheng.space"
+        >
+          Small Link
+        </Button>
       </header>
     </div>
   );
